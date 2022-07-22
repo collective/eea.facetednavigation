93ada386495985e12aa03805a90fc65fe06555e0
Alin Voinea <contact@avoinea.com>
2022-07-22 20:29:46 +0300

    Debug BBQ

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ const path = require("path");
 module.exports = [
     {
         entry: './resources/plugins.js',
+        optimization: {
+            minimize: false
+        },
         output: {
             path: path.resolve(__dirname, 'eea/facetednavigation/browser/static'),
             filename: 'faceted-jquery.min.js',
