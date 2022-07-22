51a4380479cd2bf7c240bd5ee5365a2cd88379bd
Alin Voinea <contact@avoinea.com>
2022-07-22 13:58:25 +0300

    Drop eea.jquery dependency

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,13 @@ const path = require("path");
 
 module.exports = [
     {
+        entry: './resources/plugins.js',
+        output: {
+            path: path.resolve(__dirname, 'eea/facetednavigation/browser/static'),
+            filename: 'faceted-jquery.min.js',
+        },
+    },
+    {
         entry: './resources/view.js',
         output: {
             path: path.resolve(__dirname, 'eea/facetednavigation/browser/static'),
